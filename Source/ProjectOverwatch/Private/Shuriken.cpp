d7ab3a7a9c72e3a847926374348d47147bb0473f
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2026-02-16 13:09:45 +0900

    겐지 수리검 장착

---
+++ Source/ProjectOverwatch/Private/Shuriken.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shuriken.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AShuriken::AShuriken()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Collision
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	SetRootComponent(Collision);
+	Collision->InitSphereRadius(8.f);
+	Collision->SetCollisionProfileName(TEXT("Projectile"));
+	Collision->SetNotifyRigidBodyCollision(true);
+	Collision->OnComponentHit.AddDynamic(this, &AShuriken::OnHit);
+	Collision->SetEnableGravity(false);
+
+	// Mesh
+	SkMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkMesh->SetupAttachment(Collision);
+	SkMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SkMesh->SetSimulatePhysics(false);
+	SkMesh->SetEnableGravity(false);
+
+	// Projectile Movement
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->UpdatedComponent = Collision;
+	ProjectileMovement->InitialSpeed = 1000.0f;
+	ProjectileMovement->MaxSpeed = 2000.0f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 0.f;
+}
+
+void AShuriken::FireInDirection(const FVector& ShootDir)
+{
+	ProjectileMovement->Velocity = ShootDir * ProjectileMovement->InitialSpeed;
+}
+
+void AShuriken::OnHit(UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	// 데미지 전달
+	AActor* InstigatorActor = GetInstigator();
+	AController* InstigatorController = InstigatorActor ? InstigatorActor->GetInstigatorController() : nullptr;
+
+	UGameplayStatics::ApplyPointDamage(
+		OtherActor,
+		Damage,
+		GetVelocity().GetSafeNormal(),
+		Hit,
+		InstigatorController,
+		this,
+		nullptr);
+
+	Destroy();
+}
+
+// Called when the game starts or when spawned
+void AShuriken::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 소멸 시한 설정 (투척시에만)
+	if (bUseLifeSpan) SetLifeSpan(LifeSeconds);
+}
+
+// Called every frame
+void AShuriken::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ProjectOverwatch/Public/Shuriken.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Shuriken.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class PROJECTOVERWATCH_API AShuriken : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AShuriken();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components")
+	TObjectPtr<USphereComponent> Collision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components")
+	TObjectPtr<USkeletalMeshComponent> SkMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Combat")
+	float Damage = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Combat")
+	float LifeSeconds = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Shuriken")
+	bool bUseLifeSpan = false;
+
+	// 투척 방향, 속도 세팅
+	void FireInDirection(const FVector& ShootDir);
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit);
+};
