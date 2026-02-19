4ae4207e014d3ce326ce7232b5106c05d906d465
zero <yhs5826@naver.com>
2026-02-19 17:27:21 +0900

    우양 cpp 생성

---
+++ Source/ProjectOverwatch/Public/Player_WuYang.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectOverwatchCharacter.h"
+#include "Player_WuYang.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTOVERWATCH_API APlayer_WuYang : public AProjectOverwatchCharacter
+{
+	GENERATED_BODY()
+	
+};
