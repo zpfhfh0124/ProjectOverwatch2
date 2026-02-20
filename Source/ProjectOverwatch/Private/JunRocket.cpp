6f30e4187a535d0c4d90fb1ee17d06eb5db20873
KodiakJ <kj19960526@gmail.com>
2026-02-20 20:24:12 +0900

    2626

---
+++ Source/ProjectOverwatch/Private/JunRocket.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JunRocket.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AJunRocket::AJunRocket()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	collisionComp = CreateDefaultSubobject<USphereComponent>("CollisionComp");
+	collisionComp->SetCollisionProfileName("OverlapAll");
+	collisionComp->SetSphereRadius(13);
+	SetRootComponent(collisionComp);
+	
+	bodyMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("BodyMesh");
+	bodyMeshComp->SetupAttachment(collisionComp);
+	bodyMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	bodyMeshComp->SetRelativeScale3D(FVector(0.25f));
+	
+	movementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
+	movementComp->SetUpdatedComponent(collisionComp);
+	
+	movementComp->InitialSpeed = 5000;
+	movementComp->MaxSpeed = 5000;
+	
+}
+
+// Called when the game starts or when spawned
+void AJunRocket::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AJunRocket::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
